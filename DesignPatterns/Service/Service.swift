452630f61b42a69b5c2846dc1da91e6e54f973ba
Matt Deuschle <mdeuschle@gmail.com>
2018-07-21 21:35:57 -0700

    add persistancy manager

---
+++ DesignPatterns/Service/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  DesignPatterns
+//
+//  Created by Matt Deuschle on 7/21/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import Foundation
+
+final class Service {
+    let shared = Service()
+    private init() {}
+    
+}
