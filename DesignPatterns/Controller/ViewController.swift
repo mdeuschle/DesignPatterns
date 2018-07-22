aecc9089f0200597e50264a06bd3c554b69cc6f6
Matt Deuschle <mdeuschle@gmail.com>
2018-07-21 19:13:06 -0700

    add storyboard

--- DesignPatterns/Controller/ViewController.swift
+++ DesignPatterns/Controller/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var undoBarButtonItem: UIBarButtonItem!
+    @IBOutlet private weak var trashBarButtonItem: UIBarButtonItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
